d0397e1a95c9efc239f1bd5f15cbf7939046392d
Glenn Lin <glenn.lin@easiio.com>
2022-11-15 16:56:00 +0800

    Fix: CORS Origin from *.sflow.io/pro/cc

--- packages/cli/src/Server.ts
+++ packages/cli/src/Server.ts
@@ -698,12 +698,23 @@ class App {
 		if (true || process.env.NODE_ENV !== 'production') {
 			this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
 				// Allow access also from frontend when developing
-				var allowedOrigins = ['http://127.0.0.1:8083', 'http://localhost:8083', 'http://127.0.0.1:8080', 'http://localhost:8080', 'https://sf.easiio.cn' ];
-				var origin = req.headers.origin || '';
-				if(allowedOrigins.indexOf(origin) > -1 || origin.indexOf('sflow.io') > -1 || origin.indexOf('sflow.pro') > -1){
+				// Allow access from sf.easiio.cn, *.sflow.io, *.sflow.pro, *.sflow.cc
+				const allowedOrigins = [
+					'http://127.0.0.1:8080',
+					'http://localhost:8080',
+					'http://127.0.0.1:8083',
+					'http://localhost:8083',
+					'https://sf.easiio.cn',
+				];
+				const origin = req.headers.origin ?? '';
+				if (
+					allowedOrigins.indexOf(origin) > -1 ||
+					origin.indexOf('sflow.io') > -1 ||
+					origin.indexOf('sflow.pro') > -1 ||
+					origin.indexOf('sflow.cc') > -1
+				) {
 					res.header('Access-Control-Allow-Origin', origin);
 				}
-				// res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
 				res.header('Access-Control-Allow-Credentials', 'true');
 				res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 				res.header(
--- packages/cli/src/WebhookServer.ts
+++ packages/cli/src/WebhookServer.ts
@@ -279,10 +279,26 @@ class App {
 			}),
 		);
 
-		if (process.env.NODE_ENV !== 'production') {
+		if (true || process.env.NODE_ENV !== 'production') {
 			this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
 				// Allow access also from frontend when developing
-				res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+				// Allow access from sf.easiio.cn, *.sflow.io, *.sflow.pro, *.sflow.cc
+				const allowedOrigins = [
+					'http://127.0.0.1:8080',
+					'http://localhost:8080',
+					'http://127.0.0.1:8083',
+					'http://localhost:8083',
+					'https://sf.easiio.cn',
+				];
+				const origin = req.headers.origin ?? '';
+				if (
+					allowedOrigins.indexOf(origin) > -1 ||
+					origin.indexOf('sflow.io') > -1 ||
+					origin.indexOf('sflow.pro') > -1 ||
+					origin.indexOf('sflow.cc') > -1
+				) {
+					res.header('Access-Control-Allow-Origin', origin);
+				}
 				res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 				res.header(
 					'Access-Control-Allow-Headers',
