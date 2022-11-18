26d4c18afcc0c35e2066e98752b1ef9c62b10433
Glenn Lin <glenn.lin@easiio.com>
2022-11-18 13:38:59 +0800

    Feat: Labeling

--- packages/editor-ui/src/components/mixins/titleChange.ts
+++ packages/editor-ui/src/components/mixins/titleChange.ts
@@ -20,11 +20,11 @@ export const titleChange = Vue.extend({
 				icon = '▶️';
 			}
 
-			window.document.title = `n8n - ${icon} ${workflow}`;
+			window.document.title = `Sflow - ${icon} ${workflow}`;
 		},
 
 		$titleReset() {
-			document.title = `n8n - Workflow Automation`;
+			document.title = `Sflow - Workflow`;
 		},
 
 	},
